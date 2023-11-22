cdc7b5e4af0bbf75aca39586a62a33cff06133bc
SK4 <valentin.beaussaert@laplateforme.io>
2023-11-22 14:32:23 +0100

    Modification job05

--- jour01/job05/alphabet-reverse.py
+++ jour01/job05/alphabet-reverse.py
@@ -1,3 +1,3 @@
-alphabetreverse="z y x w v u t s r q p o n m l k j i h g f e d c b a"
+alphabetreverse="a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
-print(alphabetreverse)
+print(alphabetreverse[::-1])
